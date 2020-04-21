b51b84ad973b5e05e25c1377940cb7f3318fc92f
Daniel Merys <dkmerys@gmail.com>
2020-04-21 16:18:06 -0700

    complete weather conditions interface

--- src/weather-interface.js
+++ src/weather-interface.js
@@ -1,26 +1,19 @@
+import { WeatherService } from './../src/weather-service.js';
+
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
     const city = $('#location').val();
     $('#location').val("");
 
     (async () => {
-      try {
-        // The API call is business logic.
-        let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`);
-        let jsonifiedResponse;
-        if (response.ok && response.status == 200) {
-          jsonifiedResponse = await response.json();
-        } else {
-          jsonifiedResponse = false;
-        }
-        // The getElements method is UI logic so it will need to be separated from the business logic.  
-        getElements(jsonifiedResponse);
-      } catch {
-        getElements(false);
-      }
-    })();
+        let weatherService = new WeatherService();
+        const response = await weatherService.getWeatherByCity(city);
+        console.log('response: ', response)
+        getElements(response);
+      })();
 
-    const getElements = function(response) {
+      
+    function getElements(response) {
       if (response) {
         $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
         $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
--- src/weather-service.js
+++ src/weather-service.js
@@ -1,7 +1,11 @@
 export class WeatherService {
   async getWeatherByCity(city) {
+    console.log('i am in the method')
+    // console.log('process.env.API_KEY: ', process.env.API_KEY)
     try {
       let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`);
+      console.log('i got past the await');
+
       let jsonifiedResponse;
       if (response.ok && response.status == 200) {
         jsonifiedResponse = await response.json();
@@ -10,7 +14,12 @@ export class WeatherService {
       }
       return jsonifiedResponse;
     } catch {
+      console.log('in the catch');
+
       return false;
     }
   }
 }
+
+// API_KEY = YOUR UNIQUE API KEY GOES HERE
+// OTHER_API_KEY = OTHER UNIQUE API KEY GOES HERE
